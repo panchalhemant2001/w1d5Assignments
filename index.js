935ee24ccaa21a0156787b7885e9aa19ec249132
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-13 17:58:37 +0000

    package chalk styles exercise

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var chalk = require("chalk");
 
 
-var message = "Hello " + chalk.yellow("World");
-console.log(message);
+// var message = "Hello " + chalk.yellow("World");
+// console.log(message);
+
+console.log(chalk.blue("Hello") + ' World' + chalk.red('!'));
+console.log(chalk.blue.bgRed.bold('Hello world!'));
+console.log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz' ));
+console.log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+console.log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+    ));
+
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
